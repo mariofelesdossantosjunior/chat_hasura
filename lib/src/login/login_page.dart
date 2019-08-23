43b034c58ca1c3ec9f1f72696b0c15590d8b7f87
mariofelesdossantosjunior <mario_feles@live.com>
2019-08-23 10:05:53 -0300

    Erro Hasura Conect

---
+++ lib/src/login/login_page.dart
@@ -0,0 +1,46 @@
+import 'package:chat_hasura/src/home/home_module.dart';
+import 'package:chat_hasura/src/login/login_module.dart';
+import 'package:flutter/material.dart';
+
+import 'login_bloc.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final bloc = LoginModule.to.bloc<LoginBloc>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 25),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              controller: bloc.controller,
+              decoration: InputDecoration(labelText: "NickName"),
+            ),
+            RaisedButton(
+              child: Text("Acessar"),
+              onPressed: () async{
+                await bloc.login().then((result){
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (_) => HomeModule()));
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
